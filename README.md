c4afbcc64c1793c554e56250b64711a63ac0018b
Damien Profeta <damien@ounim.fr>
2015-07-02 00:03:50 +0200

    Use timer to schedule an event in the future
    the scheduleIn variable define the number of clock to wait before the
    event has to be schedule. OCR1A define when the interrupt will be
    scheduled.
    
    The test here is to make a led blink first fast, then to slow down and
    to blink faster again.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # youpi_arduino
 This is an arduino code base to run the youpi arm
+
+
+#
+a command is received to make the motor turn.
+the timer is scheduled to interrupt when the next step has to run.
+the maximum acceptable speed of the motor is 44 pulse per second.
+A pulse is 2 commands so 88 commands per seconds.
+There are 6 motors to pilot so 528 commands per seconds so 9600 baud is well above the maximum speed possible.
+
+Arduino run at 16MHz. so it means it count up to (16 10e6)/(9.6 10e3) = 1.6 10e3 before sending each command.
--- youpi_arduino.ino
+++ youpi_arduino.ino
@@ -1,32 +1,9 @@
-// debut des mockups
-/*
-#include "stdio.h"
-
-const int OUTPUT = 1;
-void pinMode(int pin, int mode)
-{
-    printf("pinMode D%d: mode %d\n", pin, mode);
-}
-
-void digitalWrite(int pin, int number)
-{
-    printf("pin D%d : output %d\n", pin ,number);
-}
-
-void delay(int milliseconds)
-{
-    printf("attend %d millisecondes\n", milliseconds);
-}
-
-int main()
-    {
-        setup();
-        loop();
-    }
-}
-*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
 // fin des mockups
 const int D[8] =  {13, 12, 11, 10, 9, 8, 7, 6};
+long scheduleIn = 0;
+long timerstep = 1;
 void parallelOutput(int number)
 {
     for( int i= 0;i<8; ++i)
@@ -34,7 +11,6 @@ void parallelOutput(int number)
         digitalWrite(D[i], number%2);
         number = number >> 1;
     }
-    delay(2);
 }
 
 void setup() {
@@ -45,31 +21,90 @@ void setup() {
     }
     parallelOutput(0x47);
     parallelOutput(0x00);
+
+    // initialize timer1
+    cli();           // disable all interrupts
+    TCCR1A = 0;
+    TCCR1B = 0;
+ //   TCNT1  = 0;
+
+    OCR1A = 1024;            // compare match register max
+   // TCCR1B |= (1 << WGM12);
+    TCCR1B |= (1 << CS10);    // no prescaler
+ //   TCCR1B |= (1 << CS11);    // no prescaler
+    TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+    sei();
 }
 
 void loop()
 {
-    int steps = 10;
-    //Orders earch motor to rotate
-    for(int j=0; j<6; j++) {
-        //...into one direction
-        //1-0-sens moteur6-sens moteur5-sens moteur4-sens moteur3-sens moteur2-sens moteur1
-        parallelOutput(0x80);
-        //0-0-sens moteur6-sens moteur5-sens moteur4-sens moteur3-sens moteur2-sens moteur1
-        parallelOutput(0x00);
-        for(int i=0; i<steps; i++) {
-            //0-1-X-X-X-numero du moteur en binaire
-            parallelOutput(0x40+j);
-            //0-0-X-X-X-numero du moteur en binaire
-            parallelOutput(0x00+j);
-        }
+//    int steps = 10;
+//    //Orders earch motor to rotate
+//    for(int j=0; j<6; j++) {
+//        //...into one direction
+//        //1-0-sens moteur6-sens moteur5-sens moteur4-sens moteur3-sens moteur2-sens moteur1
+//        parallelOutput(0x80);
+//        //0-0-sens moteur6-sens moteur5-sens moteur4-sens moteur3-sens moteur2-sens moteur1
+//        parallelOutput(0x00);
+//        for(int i=0; i<steps; i++) {
+//            //0-1-X-X-X-numero du moteur en binaire
+//            parallelOutput(0x40+j);
+//            //0-0-X-X-X-numero du moteur en binaire
+//            parallelOutput(0x00+j);
+//        }
+//
+//        //...into the opposite direction
+//        parallelOutput(0xBF);
+//        parallelOutput(0x3F);
+//        for(int i=0; i<steps; i++) {
+//            parallelOutput(0x40+j);
+//            parallelOutput(0x00+j);
+//        }
+//    }
+}
 
-        //...into the opposite direction
-        parallelOutput(0xBF);
-        parallelOutput(0x3F);
-        for(int i=0; i<steps; i++) {
-            parallelOutput(0x40+j);
-            parallelOutput(0x00+j);
-        }
+ISR(TIMER1_OVF_VECT)
+{
+
+}
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+  static int sign = 1;
+  cli();
+  if(scheduleIn > 65536)
+  {
+    scheduleIn -= 65536;
+    if (scheduleIn > 65536)
+    {
+      OCR1A = TCNT1-1;
     }
+    else
+    {
+      OCR1A  = (TCNT1 + scheduleIn)%65536;
+    }    
+  }
+  else
+  {
+    scheduleIn = timerstep;
+    if (scheduleIn > 65536)
+    {
+      OCR1A = TCNT1-1;
+    }
+    else
+    {
+      OCR1A  = (TCNT1 + scheduleIn)%65536;
+    }
+    timerstep= timerstep+10000*sign;
+    if (timerstep > 4000000)
+    {
+      sign = -1;
+    }
+    if (timerstep < 2000)
+    {
+      sign = 1;
+    }
+    
+    digitalWrite(13, digitalRead(13) ?LOW:HIGH);   // toggle LED pin
+  }
+  sei();
 }
