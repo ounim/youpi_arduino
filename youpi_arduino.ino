6d96d261a5ca2a6f95d523168b74d7cafa61fac2
Damien Profeta <damien@ounim.fr>
2015-07-05 01:01:16 +0200

    Schedule event from Serial command

--- youpi_arduino.ino
+++ youpi_arduino.ino
@@ -1,9 +1,24 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <limits.h>
 // fin des mockups
 const int D[8] =  {13, 12, 11, 10, 9, 8, 7, 6};
-long scheduleIn = 0;
+unsigned long scheduleIn = ULONG_MAX;
 long timerstep = 1;
+
+#define WAITINGFIRSTBYTE 0
+#define WAITINGSECONDBYTE 1
+#define WAITINGID 2
+#define WAITINGLENGTH 3
+#define WAITINGINSTRUCTION 4
+#define WAITINGPARAMETERS 5
+#define WAITINGCHECKSUM 6
+char CommandReceptionState = WAITINGFIRSTBYTE;
+char currentCommandId = 0;
+char currentCommandLength = 0;
+char currentCommandInstruction = 0;
+char parametersStillToReceive = 0;
+
 void parallelOutput(int number)
 {
     for( int i= 0;i<8; ++i)
@@ -26,18 +41,76 @@ void setup() {
     cli();           // disable all interrupts
     TCCR1A = 0;
     TCCR1B = 0;
- //   TCNT1  = 0;
-
-    OCR1A = 1024;            // compare match register max
-   // TCCR1B |= (1 << WGM12);
+    OCR1A = 0;            // compare match register max
     TCCR1B |= (1 << CS10);    // no prescaler
- //   TCCR1B |= (1 << CS11);    // no prescaler
     TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
     sei();
+    
+    Serial.begin(9600);
 }
 
+void processCommand()
+{
+  scheduleIn = 3000000;
+}
 void loop()
 {
+  int inByte;
+    // if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    inByte = Serial.read();
+    switch (CommandReceptionState)
+    {
+      case WAITINGFIRSTBYTE:
+      case WAITINGSECONDBYTE:
+      {
+        if (inByte == 0xFF)
+        {
+          CommandReceptionState += 1;
+        }
+        else
+        {
+          CommandReceptionState = 0;
+        }
+        break; 
+      }
+      case WAITINGID:
+      {
+        currentCommandId = inByte;
+        CommandReceptionState += 1;
+        break;
+      }
+      case WAITINGLENGTH:
+      {
+        currentCommandLength = inByte;
+        parametersStillToReceive = currentCommandLength -2;
+        CommandReceptionState += 1;
+        break;
+      }
+      case WAITINGINSTRUCTION:
+      {
+        currentCommandInstruction = inByte;
+        CommandReceptionState += 1;
+        break;
+      }      
+      case WAITINGPARAMETERS:
+      {
+        parametersStillToReceive -=1;
+        if ( parametersStillToReceive == 0)
+        {
+          CommandReceptionState += 1;
+        }
+        break;
+      }   
+      case WAITINGCHECKSUM:
+      {
+        processCommand();
+        CommandReceptionState = 0;
+        break;
+      }       
+    }
+  }
 //    int steps = 10;
 //    //Orders earch motor to rotate
 //    for(int j=0; j<6; j++) {
@@ -71,6 +144,8 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
   static int sign = 1;
   cli();
+  if (scheduleIn == ULONG_MAX)
+    return;
   if(scheduleIn > 65536)
   {
     scheduleIn -= 65536;
@@ -85,7 +160,7 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
   }
   else
   {
-    scheduleIn = timerstep;
+
     if (scheduleIn > 65536)
     {
       OCR1A = TCNT1-1;
@@ -103,8 +178,18 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
     {
       sign = 1;
     }
-    
-    digitalWrite(13, digitalRead(13) ?LOW:HIGH);   // toggle LED pin
+    if (digitalRead(13) == LOW)
+    {
+      digitalWrite(13,HIGH);
+      scheduleIn = 300000;
+    }
+    else 
+    {
+      digitalWrite(13,LOW);
+      scheduleIn = ULONG_MAX;
+    }
+
   }
+
   sei();
 }
